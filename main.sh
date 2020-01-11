518e086324720549bd82bc087386d655530f6b78
miguelwill <miguelwill@gmail.com>
2020-01-11 09:23:02 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,8 +7,8 @@ sed '12s/AllowOverride\ None/AllowOverride\ All/' /etc/apache2/sites-enabled/def
 cat /tmp/le > /etc/apache2/sites-enabled/default-ssl
 #echo "CustomLog /proc/self/fd/1" > /etc/apache2/conf.d/other-vhosts-access-log
 rm -f /etc/apache2/conf.d/other-vhosts-access-log
+sed -i '/output_buffering = 4096/c output_buffering = Off' /etc/php5/apache2/php.ini
 
 /etc/init.d/memcached start
 
 rm -f /var/run/apache2.pid && exec apache2 -DFOREGROUND
-
